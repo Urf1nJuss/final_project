003b46872e1aa74f6b6b3bbd47a0011bdd974e31
Urf1nJuss <urfinjussoo@mail.ru>
2024-05-21 18:59:44 +0300

    comment

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
-
+21512526263
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
